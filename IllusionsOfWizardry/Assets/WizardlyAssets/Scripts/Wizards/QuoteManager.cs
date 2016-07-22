f7dc0c942cbadd6b32321b6ec635de2e2649a3f8
kstamant244 <kylestamant1@gmail.com>
2016-07-22 17:29:45 -0600

    Added Folders and Character prefab / testing scene

---
+++ IllusionsOfWizardry/Assets/WizardlyAssets/Scripts/Wizards/QuoteManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuoteManager : MonoBehaviour {
+    [SerializeField]
+    private string[] m_quotes;
+    private int m_numQuotes;
+
+    private float m_time = 0;
+    [SerializeField]
+    private float m_displayDuration = 5;
+    private bool  m_displaying;
+    [SerializeField]
+    Vector2 m_initialLocalPos;
+
+    // Use this for initialization
+    void Start () {
+        transform.localPosition = m_initialLocalPos;
+        m_numQuotes = m_quotes.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(m_displaying)
+        {
+            m_time += Time.deltaTime;
+            if(m_time >= m_displayDuration)
+            {
+                GetComponent<TextMesh>().text = "";
+                m_displaying = false;
+            }
+
+        }
+
+	}
+    //Convenience
+    public void DisplayQuote(int quoteIndex)
+    {
+        DisplayQuote(quoteIndex, m_displayDuration);
+    }
+
+    //Displays a quote[quoteIndex] for <m_displayDuration>(member variable) seconds
+    public void DisplayQuote(int quoteIndex, float duration)
+    {
+        
+        if (quoteIndex >= m_numQuotes || quoteIndex < 0)
+        {
+            Debug.Log("ERROR: QuoteManager.DisplayQuote(): quoteIndex [" + quoteIndex + "] is out of bounds! Max val:[" + (m_numQuotes - 1) + "]\n");
+        }
+        else {
+            m_displaying = true;
+            m_displayDuration = duration;
+            m_time = 0;
+            GetComponent<TextMesh>().text = m_quotes[quoteIndex];
+            
+        }
+
+
+    }
+    public void SetLocalPosition(Vector2 pos)
+    {
+        transform.localPosition = pos;
+    }
+
+    public void SetPosition(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
+
+
+}
---
+++ IllusionsOfWizardry/Assets/WizardlyAssets/Scripts/Wizards/WizardCharacter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardCharacter : MonoBehaviour {
+    [SerializeField] private QuoteManager quotes;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+        //GetComponentInChildren<TextMesh>().text = m_quotes[0];
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            quotes.DisplayQuote(1);
+        }
+       
+    }
+
+
+    //private void SayQuote()
+
+
+
+}
