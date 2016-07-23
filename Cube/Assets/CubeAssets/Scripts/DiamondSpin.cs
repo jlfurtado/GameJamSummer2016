56bbf304ab0deb8e0756da2172c9d650f04c8c05
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-07-23 13:10:04 -0600

    Rotating Diamonds :D

---
+++ Cube/Assets/CubeAssets/Scripts/DiamondSpin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondSpin : MonoBehaviour {
+
+    [SerializeField]
+    private int m_rotateSpeed = 60; // rotate speed in degrees
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(0, m_rotateSpeed * Time.deltaTime, 0);
+	}
+}
