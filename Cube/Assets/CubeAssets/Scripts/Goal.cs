3de676e609cff0791ece0a8d6b2c11a58427b25f
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-07-23 17:01:41 -0600

    Added Goal Asset, moved .objs and .cs files around. Goal trigger does not work.

---
+++ Cube/Assets/CubeAssets/Scripts/Goal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Code for detecting cube?
+        // Or should cube detect the goal?
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("Yay! Touched the goal!");
+        }
+    }
+}
