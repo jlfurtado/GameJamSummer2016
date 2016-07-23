51e8864bca9dc74bda2a0b8f53f16320a4c5b68b
kstamant244 <kylestamant1@gmail.com>
2016-07-23 10:28:22 -0600

    Made cube rotatable

---
+++ Cube/Assets/CubeAssets/Scripts/CubeControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+public class CubeControl : MonoBehaviour {
+
+
+    Vector3 m_up;
+
+	// Use this for initialization
+	void Start () {
+        m_up = new Vector3(0, 1.0f, 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(m_up, CrossPlatformInputManager.GetAxis("Horizontal"));
+
+
+	}
+}
