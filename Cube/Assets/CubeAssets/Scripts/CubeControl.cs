90876ff06d6579362b477531b5023cd3e8ad24be
kstamant244 <kylestamant1@gmail.com>
2016-07-23 12:00:12 -0600

    Added more controls, and jumping for cube

--- Cube/Assets/CubeAssets/Scripts/CubeControl.cs
+++ Cube/Assets/CubeAssets/Scripts/CubeControl.cs
@@ -6,36 +6,78 @@ public class CubeControl : MonoBehaviour {
     Vector3 m_previousPos;
     Vector3 m_up;
     Vector3 m_heading;
-
+    Vector3 m_right;
     Rigidbody m_rigidBody;
-
+    private Transform m_GroundCheck;
     [SerializeField] private float m_moveSpeed = 10;
     [SerializeField]
     private float m_rotateSpeed = 10;
-    private bool m_collided;
+    [SerializeField]
+    private float m_jumpSpeed = 10;
+    [SerializeField]
+    private float m_groundDetectRadius = .10f;
+    [SerializeField]
+    private LayerMask m_WhatIsGround;
+
+
+    private int m_colliderListLen = 0;
+
+    private bool m_collided = false;
+    private bool onGround = false;
     // Use this for initialization
     void Start () {
         m_up = new Vector3(0, 1.0f, 0);
         m_heading = new Vector3(0, 0, 1.0f);
         m_rigidBody = GetComponent<Rigidbody>();
         m_previousPos = transform.position;
+        m_right = Vector3.Cross(m_up, m_heading);
+        m_GroundCheck = transform.Find("GroundCheck");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject != this.gameObject)
+        if(other.gameObject.layer == m_WhatIsGround)
         {
             m_rigidBody.AddForce(-m_heading * m_moveSpeed);
-
+            
         }
        
+        if(other.CompareTag("Collectible"))
+        {
+            Destroy(other.gameObject);
+        }
+
     }
 
     void FixedUpdate()
     {
+        onGround = false;
+        Collider[] colliders = Physics.OverlapSphere(m_GroundCheck.position, m_groundDetectRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            m_colliderListLen = colliders.Length;
+            if (colliders[i].gameObject != gameObject)
+            {
+         
+                onGround = true;
+            }
+
+        }
+
         transform.Rotate(m_up, CrossPlatformInputManager.GetAxis("Horizontal") * m_rotateSpeed * Time.deltaTime);
         m_heading =  Quaternion.Euler(0, CrossPlatformInputManager.GetAxis("Horizontal") * m_rotateSpeed * Time.deltaTime, 0) * m_heading;
+        m_right = Quaternion.Euler(0, CrossPlatformInputManager.GetAxis("Horizontal") * m_rotateSpeed * Time.deltaTime, 0) * m_right;
+
         m_rigidBody.AddForce(CrossPlatformInputManager.GetAxis("Vertical") *  m_heading * m_moveSpeed);
+
+        m_rigidBody.AddForce(CrossPlatformInputManager.GetAxis("Strafe") * m_right * m_moveSpeed);
+
+        if (onGround && CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            m_rigidBody.AddForce(m_up * m_jumpSpeed);
+        }
+
+
     }
 
 
