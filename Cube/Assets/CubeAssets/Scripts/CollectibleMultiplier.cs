251892aacc91dac25d1d4f37c64539e0a2a55bbf
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-07-23 19:54:59 -0600

    Added Multiplier, and relavant text. Text for said multiplier doesn't work yet.

---
+++ Cube/Assets/CubeAssets/Scripts/CollectibleMultiplier.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleMultiplier : MonoBehaviour {
+
+    ScoreManager m_scoreManager;
+
+    [SerializeField]
+    private GameObject m_objWithScoreManager;
+    [SerializeField]
+    private int m_multiplier;
+
+	// Use this for initialization
+	void Start () {
+        m_scoreManager = m_objWithScoreManager.GetComponent<ScoreManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Multiplier"))
+        {
+            Destroy(other.gameObject);
+            m_scoreManager.AddMultiplier(m_multiplier);
+            //Debug.Log("Score: " + m_score + "\n");
+        }
+
+    }
+}
--- Cube/Assets/CubeAssets/Scripts/ScoreManager.cs
+++ Cube/Assets/CubeAssets/Scripts/ScoreManager.cs
@@ -9,6 +9,7 @@ public class ScoreManager : MonoBehaviour {
     Text  m_text; // score
     [SerializeField]
     private int m_score = 0; // score for collecting diamonds for now i guess :) thanks
+    private int m_scoreMultiplier = 1;
                              // Use this for initialization
     void Start () {
         m_cc = GetComponentInParent<CubeControl>();
@@ -17,10 +18,14 @@ public class ScoreManager : MonoBehaviour {
 	
     public void AddScore(int score)
     {
-       m_score = m_score + score;// = m_cc.GetScore().ToString();
+       m_score = m_score + (score * m_scoreMultiplier);// = m_cc.GetScore().ToString();
         m_text.text = m_score.ToString();
     }
 
+    public void AddMultiplier(int multipler)
+    {
+        m_scoreMultiplier = m_scoreMultiplier * multipler;
+    }
 
 	// Update is called once per frame
 	void Update () {
