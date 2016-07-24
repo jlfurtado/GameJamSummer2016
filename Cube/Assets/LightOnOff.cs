98acb5cb5828526891e35962af81f3c7c7a33e7d
kstamant244 <kylestamant1@gmail.com>
2016-07-24 11:31:04 -0600

    Fixed wall and lighting

---
+++ Cube/Assets/LightOnOff.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightOnOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Light>().enabled = false;
+        GetComponent<Light>().enabled = true;
+    }
+	
+	
+}
---
+++ Cube/Assets/Refresh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Refresh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+    void Awake()
+    {
+        enabled = false;
+        enabled = true;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
