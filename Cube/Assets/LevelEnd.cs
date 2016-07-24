0f53ef19ae5f71ce4ad95b04aac63959d33afb6e
Justin Furtado <justin.l.furtado@gmail.com>
2016-07-24 10:32:58 -0600

    Basic level-to-level stuffs

---
+++ Cube/Assets/LevelEnd.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelEnd : MonoBehaviour {
+
+    private float m_animationTimer = 0.0f;
+    private bool m_animating = false;
+    [SerializeField]
+    private string m_nextLevelString = "";
+    [SerializeField]
+    private float m_totalAnimationTime = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (m_animating)
+        {
+            m_animationTimer += Time.deltaTime;
+            if (m_animationTimer > m_totalAnimationTime)
+            {
+                LoadNextLevel();
+            }
+        }   
+	}
+
+    void LoadNextLevel()
+    {
+        if (!m_nextLevelString.Equals(""))
+        {
+            SceneManager.LoadScene(m_nextLevelString);
+        }
+        else
+        {
+            Debug.Log("Warning: You touched a goal that was not set up to load the next level!\n");
+        }
+
+        m_animating = false;
+        m_animationTimer = 0.0f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if colliding with CUBE
+        if (other.CompareTag("Player"))
+        {
+            m_animating = true;
+            m_animationTimer = 0.0f;
+        }
+    }
+}
