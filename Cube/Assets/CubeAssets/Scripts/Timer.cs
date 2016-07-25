45442e05f474fa3e641842762574cc6de93961c4
Justin Furtado <justin.l.furtado@gmail.com>
2016-07-24 19:25:47 -0600

    Add timers that cause loss when out of time

---
+++ Cube/Assets/CubeAssets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    private int m_totalSeconds = 30;
+    private int m_secondsRemaining = 0;
+    private float m_secondTimer = 0.0f;
+
+    [SerializeField]
+    Text m_timeRemainingText;
+
+	// Use this for initialization
+	void Start () {
+        m_secondsRemaining = m_totalSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_secondTimer += Time.deltaTime;
+
+        if (m_secondTimer > 1.0f)
+        {
+            m_secondTimer -= 1.0f;
+            m_secondsRemaining -= 1;
+        }
+
+        m_timeRemainingText.text = "Time Remaining: " + m_secondsRemaining.ToString() + "s!";
+        if (m_secondsRemaining <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+	}
+}
