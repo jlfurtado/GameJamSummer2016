c1fe8660420bfdc9fced497608a8bc39d3bd7937
kstamant244 <kylestamant1@gmail.com>
2016-07-24 23:21:15 -0600

    Added the actual high score thing

---
+++ Cube/Assets/CubeAssets/Scripts/HighScoreDisplayer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HighScoreDisplayer : MonoBehaviour
+{
+    [SerializeField] Text[] m_texts;
+    [SerializeField] bool clear = false;
+
+    void Start()
+    {
+        int sizeOfArray = m_texts.Length;
+
+        int curRank = 1;
+
+        bool exists = true;
+
+        if (clear)
+        {
+
+            while (curRank < sizeOfArray)
+            {
+
+                PlayerPrefs.SetInt("HighScore" + curRank, 0);
+
+                curRank++;
+            }
+            curRank = 1;
+
+        }
+            while (exists && curRank < sizeOfArray) 
+        {
+            
+            if (PlayerPrefs.HasKey("HighScore" + curRank))
+            {
+                Debug.Log("Has highscore: [" + curRank + "] val: [" + PlayerPrefs.GetInt("HighScore" + curRank));
+                m_texts[curRank].text = PlayerPrefs.GetInt("HighScore" + curRank) + " ";
+            }
+            else
+            {
+                exists = false;
+            }
+            curRank++;
+        } 
+
+    }
+
+}
---
+++ Cube/Assets/CubeAssets/Scripts/HighScoreSystem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreSystem : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        int curRank = 0;
+        int tempScore = PlayerPrefs.GetInt("PlayerScore");
+        int tempScore2;
+        bool exists = false;
+
+        while (!exists && curRank < 10)
+        {
+            curRank++;
+            Debug.Log("Loop " + curRank);
+           
+            if (PlayerPrefs.HasKey("HighScore" + curRank))
+            {
+                Debug.Log("HighScoreSystem HasKey HighScore" + curRank);
+                if (tempScore > PlayerPrefs.GetInt("HighScore" + curRank))
+                {
+                    tempScore2 = PlayerPrefs.GetInt("HighScore"+curRank);
+                    PlayerPrefs.SetInt("HighScore" + curRank, tempScore);
+                    tempScore = tempScore2;
+                    Debug.Log("Added: " + curRank);
+                    continue;
+                }
+                Debug.Log("Exists = false: " + curRank);
+                exists = false;
+            }
+            else
+            {
+                Debug.Log("HighScoreSystem Added HighScore" + curRank);
+                PlayerPrefs.SetInt("HighScore" + curRank, PlayerPrefs.GetInt("PlayerScore"));
+                exists = true;
+            }
+
+
+        }
+
+        
+    }
+	
+}
