ee8736eb02b9186f0646dff902ca0882127aef1a
kstamant244 <kylestamant1@gmail.com>
2016-07-25 02:27:59 -0600

    Added level red2 and fixed cone ai, not done

---
+++ Cube/Assets/DetectCube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class DetectCube : MonoBehaviour {
+
+    [SerializeField]
+    AICharacterControl m_coneAi;
+    [SerializeField]
+    NavMeshAgent m_coneAgent;
+    [SerializeField]
+    Transform coneTrans;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cube"))
+        {
+            m_coneAi.enabled = true;
+            m_coneAgent.enabled = true;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Cube/Assets/WhoWon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class WhoWon : MonoBehaviour {
+
+   
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Cone"))
+        {
+
+            SceneManager.LoadScene("LevelYellow");
+            //enabled = false;
+        }
+        else if(other.CompareTag("Cube"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+    }
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
