27f11440aa05c7d38de8ba57749401a85bb62c1b
HiddenDeathOrb <hiddendeathorb@gmail.com>
2016-07-24 22:51:55 -0600

    Added script and png. Main Menu scene needs updating.

---
+++ Cube/Assets/CubeAssets/Scripts/PressKeyToStartFlash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressKeyToStartFlash : MonoBehaviour {
+
+    [SerializeField]
+    float m_flashDuration = 1.0f;
+
+    float m_currentTime = 0.0f;
+    bool m_isOn = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_currentTime += Time.deltaTime;
+        if (m_currentTime >= m_flashDuration)
+        {
+            GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+            m_currentTime = 0.0f;
+        }
+	}
+}
